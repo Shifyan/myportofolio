70d0c9f61df7fda85337569bd56284480eb3db00
Shifyannn <asifyan01@gmail.com>
2024-03-20 13:59:23 +0700

    Penambahan text pada hero section

---
+++ asset/typed/typed.js
@@ -0,0 +1,13 @@
+const nameTyped = new Typed('#name-typed', {
+  strings: ['SHIFYAN ALMUSTAFID', 'WEB DEVELOPER', 'PONOROGO'],
+  typeSpeed: 100,
+  startDelay: 1000,
+  backSpeed: 50,
+  backDelay: 2000,
+  smartBackspace: true,
+  loop: true,
+  loopCount: 100,
+  cursorSpeed: 500,
+  autoInsertCss: true,
+  cursorChar: '|'
+});
