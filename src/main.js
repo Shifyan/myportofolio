84437e83ac1df2e2be1398eba05dfaab52a75081
Shifyannn <asifyan01@gmail.com>
2024-03-22 16:24:40 +0700

    penambahan fitur pada section my project

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
-// const toHeroLink = document.getElementById('toHero');
-// const toStackLink = document.getElementById('toStack');
+const codeIcon = document.getElementById('code-icon');
 
-// toHeroLink.addEventListener('click', event => {
-//   event.preventDefault();
-//   ScrollReveal().reveal('#hero', { duration: 500 });
-// });
+codeIcon.addEventListener('mouseenter', e => {
+  const codeText = document.getElementById('code-text');
+  codeText.classList.add('opacity-100');
+  codeText.classList.add('transition');
+  codeText.classList.add('duration-500');
+});
 
-// toStackLink.addEventListener('click', event => {
-//   event.preventDefault();
-//   ScrollReveal().reveal('#tech-stack', { duration: 500 });
-// });
+codeIcon.addEventListener('mouseleave', e => {
+  const codeText = document.getElementById('code-text');
+  codeText.classList.remove('opacity-100');
+  codeText.classList.remove('transition');
+  codeText.classList.remove('duration-500');
+});
 
-// console.log(toStackLink);
-// console.log(toHeroLink)
+console.log(codeIcon);
