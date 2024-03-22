a06d1bb0d88fab27f52ab0202a3477ccb2c4e7a5
Shifyannn <asifyan01@gmail.com>
2024-03-22 12:46:23 +0700

    Penambahan my project section

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// const toHeroLink = document.getElementById('toHero');
+// const toStackLink = document.getElementById('toStack');
+
+// toHeroLink.addEventListener('click', event => {
+//   event.preventDefault();
+//   ScrollReveal().reveal('#hero', { duration: 500 });
+// });
+
+// toStackLink.addEventListener('click', event => {
+//   event.preventDefault();
+//   ScrollReveal().reveal('#tech-stack', { duration: 500 });
+// });
+
+// console.log(toStackLink);
+// console.log(toHeroLink)
