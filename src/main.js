f6afadeb712ac4d0f0eed24074a119365ad40874
Shifyannn <asifyan01@gmail.com>
2024-03-22 21:33:47 +0700

    Penambahan section quote

--- src/main.js
+++ src/main.js
@@ -14,4 +14,22 @@ codeIcon.addEventListener('mouseleave', e => {
   codeText.classList.remove('duration-500');
 });
 
-console.log(codeIcon);
+document.addEventListener('DOMContentLoaded', () => {
+  const quote = document.getElementById('quote');
+  const cite = document.getElementById('author');
+  async function updateQuote() {
+    // Fetch a random quote from the Quotable API
+    const response = await fetch('https://api.quotable.io/random');
+    const data = await response.json();
+    if (response.ok) {
+      // Update DOM elements
+      quote.textContent = data.content;
+      cite.textContent = `-${data.author}`;
+    } else {
+      quote.textContent = 'An error occured';
+      console.log(data);
+    }
+  }
+  updateQuote();
+});
+// console.log(codeIcon);
