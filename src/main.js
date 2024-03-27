4f9c4832573e03f093df3dee0a9fdda19643fd26
Shifyannn <asifyan01@gmail.com>
2024-03-27 22:29:29 +0700

    Penambahan Fitur

--- src/main.js
+++ src/main.js
@@ -6,19 +6,24 @@ window.addEventListener('scroll', () => {
     navbar.classList.replace('bg-white/75', 'bg-white');
   }
 });
-
-const codeIcon = document.querySelectorAll('.code-icon');
-const codeText = document.querySelectorAll('.code-text');
-
+let codeIcon = '';
+let codeText = document.querySelectorAll('.code-text');
+const windowWidth = window.innerWidth;
+if (windowWidth >= 768) {
+  console.log(windowWidth);
+  codeIcon = document.querySelectorAll('.code-icon');
+  console.log(codeIcon);
+}
+console.log(codeIcon);
 codeIcon.forEach((el, i) => {
   el.addEventListener('mouseenter', () => {
-    codeText[i].classList.add('opacity-100');
+    codeText[i].classList.add('md:opacity-100');
     codeText[i].classList.add('transition');
     codeText[i].classList.add('duration-500');
   });
 
   el.addEventListener('mouseleave', () => {
-    codeText[i].classList.remove('opacity-100');
+    codeText[i].classList.remove('md:opacity-100');
     codeText[i].classList.remove('transition');
     codeText[i].classList.remove('duration-500');
   });
